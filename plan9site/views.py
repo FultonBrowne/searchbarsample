b40b3463ebfb2ac1f22dac3a716e19921e9477d9
Fulton Browne <fultonbrowne@pm.me>
2023-10-06 17:18:22 -0500

    Update views.py

--- plan9site/views.py
+++ plan9site/views.py
@@ -5,12 +5,14 @@ from .forms import ProductSearchForm
 
 def product_search(request):
     form = ProductSearchForm(request.GET)
-    products = Product.objects.all()
+    products = None
+    products_cache = Product.objects.all()
 
     if form.is_valid():
         search_query = form.cleaned_data.get('search_query')
+        print("search", search_query)
         if search_query:
-            products = products.filter(name__icontains=search_query)
+            products = products_cache.filter(name__icontains=search_query)
 
     context = {
         'products': products,
